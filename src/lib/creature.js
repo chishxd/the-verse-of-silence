025fd8bf657f665a545314dc790e5f44514eb0b4
Chinmay Shet <chishxd@gmail.com>
2025-10-07 18:30:04 +0530

    Added: First creature 'glimmerkin'

---
+++ src/lib/creature.js
@@ -0,0 +1,7 @@
+export const creature = {
+	name: 'Glimmerkin',
+	image: '/glimmerkin.png',
+	description:
+		'A small, crystalline creature that hums a soft tune. It shivers and hides its face when you approach, its large, luminous eye wide with fear. Sudden flashes seem to startle it into fleeing.',
+	vulnerabilities: ['light']
+};
