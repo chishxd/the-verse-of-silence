7ed935f9e35dd7cc688dfc5f836801f01ba07e0f
Chinmay Shet <chishxd@gmail.com>
2025-10-07 17:39:00 +0530

    Added: Definition of initial echoes

---
+++ src/lib/elements.js
@@ -0,0 +1,28 @@
+export const elements = {
+	'Flickering Echo': {
+		icon: '✨',
+		description: 'An echo of pure energy and light. The memory of a star.',
+		tags: ['light', 'energy', 'non-lethal']
+	},
+	'Sighing Echo': {
+		icon: '💨',
+		description: 'The last breath of a dying wind. An echo of motion, freedom, and force',
+		tags: ['air', 'wind', 'force', 'non-lethal']
+	},
+	'Weeping Echo': {
+		icon: '💧',
+		description: 'The tear of a petrified god. An echo of sorrow, life, and flowing water',
+		tags: ['water', 'liquid', 'wet', 'life', 'non-lethal']
+	},
+	'Grumbling Echo': {
+		icon: '🗿',
+		description: 'The deep hum of the sleeping earth. An echo of stability, stone, and permanence',
+		tags: ['earth', 'solid', 'stone', 'defense', 'non-lethal']
+	},
+	'Clinging Echo': {
+		icon: '🌿',
+		description:
+			'The stubborn grasp of a single vine. An echo of nature, persistence, and connection',
+		tags: ['organic', 'sticky', 'nature', 'non-lethal']
+	}
+};
