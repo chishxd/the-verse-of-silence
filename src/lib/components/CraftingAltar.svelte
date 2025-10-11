3c6405234783190d5b5f86e9ba375df7dddb3cb8
Chinmay Shet <chishxd@gmail.com>
2025-10-11 13:04:12 +0530

    feat(engine): dragging elements adds new element to inventory

--- src/lib/components/CraftingAltar.svelte
+++ src/lib/components/CraftingAltar.svelte
@@ -1,15 +1,53 @@
+<script>
+	import { gameState } from '$lib/stores';
+	import { combine, useSpell } from '$lib/engine';
+	import { spellbook } from '$lib/spellbook';
+
+	let heldItem = null; //Stores the first item dropped in altar
+	let feedbackMessage = 'Weave the Echoes....';
+
+	function handleOnDrop(event) {
+		event.preventDefault();
+
+		let droppedItem = event.dataTransfer.getData('text/plain');
+		// console.log(`Dropped "${droppedItem}" on the Altar!`);
+
+		if (!heldItem) {
+			//If there is no heldItem, current dropped one is held
+			heldItem = droppedItem;
+			feedbackMessage = `Dropped ${heldItem} to Altar, add another echo...`;
+			return;
+		}
+		const newSpellName = combine(heldItem, droppedItem);
+
+		if (!newSpellName) {
+			feedbackMessage = 'The echoes repel each other. This combination is not possible.';
+		}
+
+		const newSpellData = spellbook[newSpellName];
+
+		// Use the function syntax for updating the store to ensure reactivity
+		gameState.update((currentState) => {
+			currentState.inventory[newSpellName] = spellbook[newSpellName];
+			return currentState;
+		});
+		feedbackMessage = `Success! You have woven '${newSpellName}'!`;
+
+		// To reset heldItem and feedback messagge to default
+		setTimeout(() => {
+			heldItem = null;
+			feedbackMessage = 'Weave the Echoes...';
+		}, 3000);
+	}
+</script>
+
 <div
 	role="group"
 	class="pixel-frame frame-wood mb-5 flex h-64 w-full items-center justify-center p-4"
 	ondragover={(event) => {
 		event.preventDefault();
 	}}
-	ondrop={(event) => {
-		event.preventDefault();
-
-		let itemName = event.dataTransfer.getData('text/plain');
-		console.log(`Dropped "${itemName}" on the Altar!`);
-	}}
+	ondrop={handleOnDrop}
 >
-	<p class="animate-pulse font-pixel text-gray-500/50">Weave the Echoes...</p>
+	<p class="animate-pulse font-pixel text-gray-500/50">{feedbackMessage}</p>
 </div>
